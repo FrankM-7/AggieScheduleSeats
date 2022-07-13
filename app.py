2ee47f28fbb79a6f4bb1bdb856ef61a2de3d3336
Frank Martinez <frankm-7@tamu.edu>
2022-07-12 23:39:50 -0500

    head

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return { 'data' : 'data'}
-
 @app.route("/something")
 def something():
     return { 'somedata' : [{'some' : 'data'}, {'some' : 'otherdata'}] }
